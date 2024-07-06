fed67886ad0a5d9953854a9cc0f6b95d50ec5414
vedant-bari <vedantbari40@gmail.com>
2024-07-06 22:41:14 +0530

    updated production files

--- piedpiper/config/production.py
+++ piedpiper/config/production.py
@@ -8,12 +8,12 @@ class Production(Common):
     # Site
     # https://docs.djangoproject.com/en/2.0/ref/settings/#allowed-hosts
     ALLOWED_HOSTS = ["*"]
-    INSTALLED_APPS += ("gunicorn", )
+    # INSTALLED_APPS += ("gunicorn", )
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/2.0/howto/static-files/
     # http://django-storages.readthedocs.org/en/latest/index.html
-    INSTALLED_APPS += ('storages',)
+    # INSTALLED_APPS += ('storages',)
     #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     #AWS_ACCESS_KEY_ID = os.getenv('DJANGO_AWS_ACCESS_KEY_ID')
@@ -27,6 +27,6 @@ class Production(Common):
     # https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching#cache-control
     # Response can be cached by browser and any intermediary caches (i.e. it is "public") for up to 1 day
     # 86400 = (60 seconds x 60 minutes x 24 hours)
-    AWS_HEADERS = {
-        'Cache-Control': 'max-age=86400, s-maxage=86400, must-revalidate',
-    }
+    # AWS_HEADERS = {
+        # 'Cache-Control': 'max-age=86400, s-maxage=86400, must-revalidate',
+    # }
