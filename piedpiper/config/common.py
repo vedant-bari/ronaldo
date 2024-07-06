06cfe612ac3daa5802b6fd729bf2930dceeb4243
vedant-bari <vedantbari40@gmail.com>
2024-07-06 22:37:23 +0530

    updated files

--- piedpiper/config/common.py
+++ piedpiper/config/common.py
@@ -146,7 +146,7 @@ class Common(Configuration):
         'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     )
 
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     # Media files
     MEDIA_ROOT = join(os.path.dirname(BASE_DIR), 'media')
     MEDIA_URL = '/media/'
